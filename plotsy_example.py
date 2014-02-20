106c9c11128231517f52e111fc3b2f3f2c6d017e
Malcolm Forde <MalcolmMcGinn@gmail.com>
2014-02-20 10:13:42 -0800

    Update plotsy_example.py

--- plotsy_example.py
+++ plotsy_example.py
@@ -10,4 +10,13 @@ for i in range(6):
 for i in range(6):
 	graph.plot([i, i**2])
 
+for i in range(8):
+    graph.plot([28 - i, i])
+   
+for i in range(29):
+    graph.plot([i, 29], "%")
+    graph.plot([i, 0], "%")
+    graph.plot([29, i], "%")
+    graph.plot([0, i], "%")
+
 graph.draw()
